5f881f4ee2ae07a9f94ea0dc461c0641e797829e
celeste tene <mbogoumcelesta@gmail.com>
2026-04-30 22:24:50 +0200

    Ajout du film Sarah_bellows dans les films eviidents

--- data.js
+++ data.js
@@ -1,5 +1,13 @@
-const films = {
-     evident: {
-        
-     }
-}
+const films = [
+     evident: [
+         {
+      id: "sarah_bellows",
+      title: "Histoires Terrifiantes à Raconter dans le Noir",
+      answers: ["histoires terrifiantes", "scary stories to tell in the dark", "histoires terrifiantes à raconter dans le noir"],
+      hint: "Un livre maudit qui écrit ses propres histoires...",
+      object: "book",
+      color: 0x8b0000,
+      points: 100,
+    },
+    {
+
