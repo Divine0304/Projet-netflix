e0f85a286dad8edb12b8394c0cbb528c9bc3f15c
OMATY Divine Marlène <divineomaty@gmail.com>
2026-05-01 18:08:14 +0200

    Implantation des elements necessaires

---
+++ scenes/libraryScene.js
@@ -0,0 +1,6 @@
+import * as THREE from "three";
+import films from "../data/films.js";
+import libraryObjects from "./libraryObjects.js";
+import libraryQuiz from "./libraryQuiz.js";
+import libraryScore from "./libraryScore.js";
+import resize from "../helpers/resize.js";
